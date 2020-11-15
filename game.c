c1775fb020560d27d5162f39a0295e858e22a3b3
Yu Ting <leeyutingsg@gmail.com>
2020-11-15 13:32:33 +0800

    added multiple enemies, added num of enemies killed, fixed some bugs

--- game.c
+++ game.c
@@ -11,6 +11,7 @@
 #define TIME 10
 #define SCORE 10
 #define LIVES 10
+#define KILLS 10
 #define word 100
 
 #define MAXC 20
@@ -27,6 +28,8 @@ char scorebuffer[SCORE];
 int lives;
 char lifebuffer[LIVES];
 char wordbuf[word];
+int numkilled;
+char killedbuffer[KILLS];
 
 
 CP_Color fontColour;
@@ -35,25 +38,27 @@ int y, i;
 
 char userinput[MAXC];
 char* ui = userinput;
-int n1, n2;
+int n1, n2, n3;
 
 int nextchar = 0;
 int numofcorrect = 0;
 int x, x1, x2, x3;
 char* wordchosen;
 char* wordchosen2;
+char* wordchosen3;
 
 
-float enemyx1, enemyy1, enemyx2, enemyy2;
+float enemyx1, enemyy1, enemyx2, enemyy2, enemyx3, enemyy3;
 float gridwidth, gridheight;
 float velx;
 
-char string1[MAXC], string2[MAXC];
+char string1[MAXC], string2[MAXC], string3[MAXC];
 
 char* pstr1;
 char* pstr2;
+char* pstr3;
 int numofchar;
-int intvalue1[MAXC], intvalue2[MAXC];
+int intvalue1[MAXC], intvalue2[MAXC], intvalue3[MAXC];
 float playerx, playery;
 
 struct WORDS
@@ -83,16 +88,22 @@ void game_init(void)
 	//set random word
 	x1 = RandomWord();
 	x2 = RandomWord();
+	x3 = RandomWord();
 	
 	//set coordinates of enemy/player
 	enemyx1 = 28;
-	enemyy1 = 10;
+	enemyy1 = 5;
 	enemyx2 = 28;
-	enemyy2 = 5;
+	enemyy2 = 10;
+	enemyx3 = 28;
+	enemyy3 = 15;
 	playerx = 2;
 	playery = 10;
 	velx = -0.05f;
 	lives = 5;
+	numkilled = 0;
+	memset(userinput, 0, MAXC * sizeof(char));
+	nextchar = 0;
 
 	
 
@@ -118,15 +129,18 @@ void game_update(void)
 	DisplayTime(time, gridwidth, gridheight);
 	DisplayScore(score, gridwidth, gridheight);
 	DisplayLives(lives, gridwidth, gridheight);
+	DisplayNumberOfEnemiesKilled(numkilled, gridwidth, gridheight);
 	
 	Drawplayer(playerx, playery, gridwidth, gridheight);
 	
 	//use randomized value to obtain a word from the wordlist and store it in strings
 	wordchosen = wordlist(x1);
 	wordchosen2 = wordlist(x2);
+	wordchosen3 = wordlist(x3);
 	
 	n1 = numofcharacters(x1);
 	n2 = numofcharacters(x2);
+	n3 = numofcharacters(x3);
 
 	for (int test = 0; test < n1; test++)
 	{
@@ -136,41 +150,51 @@ void game_update(void)
 	{
 		string2[test] = *(wordchosen2 + test);
 	}
+	for (int test = 0; test < n3; test++)
+	{
+		string3[test] = *(wordchosen3 + test);
+	}
 
 	pstr1 = string1;
 	pstr2 = string2;
+	pstr3 = string3;
 
 	Drawenemy(pstr1, enemyx1, enemyy1, gridwidth, gridheight);
 	Drawenemy(pstr2, enemyx2, enemyy2, gridwidth, gridheight);
+	Drawenemy(pstr3, enemyx3, enemyy3, gridwidth, gridheight);
 	enemyx1 += velx;
 	enemyx2 += velx;
+	enemyx3 += velx;
 	
 	ConvertWordToInt();
 	
 	Keyinput();
 	//displays words typed on screen
 	CP_Font_DrawText(ui, 500, 100);
+
+	PlayerMovement();
 	
 	//minus life if enemy reaches playerline
 	if ((int)enemyx1 == (int)playerx)
 	{
-		lives -= 1;
+		lives--;
 		if (lives == 0)
 		{
 			CP_Engine_SetNextGameState(gameover_init, gameover_update, gameover_exit);
 			time = 0;
 			score = 0;
+			numkilled = 0;
 			memset(userinput, 0, MAXC * sizeof(char));
 			memset(string1, 0, 20 * sizeof(char));
+			memset(string2, 0, 20 * sizeof(char));
+			memset(string3, 0, 20 * sizeof(char));
 		}
 		else
 		{
 			numofcorrect = 0;
-			memset(userinput, 0, MAXC * sizeof(char));
 			memset(string1, 0, 20 * sizeof(char));
-			nextchar = 0;
 			enemyx1 = 28;
-			enemyy1 = 10;
+			enemyy1 = 5;
 			x1 = RandomWord();
 
 		}
@@ -184,19 +208,44 @@ void game_update(void)
 			CP_Engine_SetNextGameState(gameover_init, gameover_update, gameover_exit);
 			time = 0;
 			score = 0;
+			numkilled = 0;
 			memset(userinput, 0, MAXC * sizeof(char));
+			memset(string1, 0, 20 * sizeof(char));
 			memset(string2, 0, 20 * sizeof(char));
+			memset(string3, 0, 20 * sizeof(char));
 		}
 		else
 		{
 			numofcorrect = 0;
-			memset(userinput, 0, MAXC * sizeof(char));
 			memset(string2, 0, 20 * sizeof(char));
-			nextchar = 0;
 			enemyx2 = 28;
-			enemyy2 = 5;
+			enemyy2 = 10;
 			x2 = RandomWord();
 			
+
+		}
+	}
+	if ((int)enemyx3 == (int)playerx)
+	{
+		lives -= 1;
+		if (lives == 0)
+		{
+			CP_Engine_SetNextGameState(gameover_init, gameover_update, gameover_exit);
+			time = 0;
+			score = 0;
+			numkilled = 0;
+			memset(userinput, 0, MAXC * sizeof(char));
+			memset(string1, 0, 20 * sizeof(char));
+			memset(string2, 0, 20 * sizeof(char));
+			memset(string3, 0, 20 * sizeof(char));
+		}
+		else
+		{
+			numofcorrect = 0;
+			memset(string3, 0, 20 * sizeof(char));
+			enemyx3 = 28;
+			enemyy3 = 15;
+			x3 = RandomWord();
 
 		}
 	}
@@ -206,9 +255,9 @@ void game_update(void)
 	{
 		//check if they are the same
 		int lenofinput = numofcharactersinput();
-		if (playery == 10)
+		
+		if (playery == 5)
 		{
-
 			for (int check = 0; check < lenofinput; check++)
 			{
 				if (intvalue1[check] == (int)userinput[check])
@@ -223,14 +272,19 @@ void game_update(void)
 				{
 					numofcorrect = 0;
 					score++;
+					numkilled++;
 					memset(userinput, 0, MAXC * sizeof(char));
 					memset(string1, 0, MAXC * sizeof(char));
 					nextchar = 0;
 					enemyx1 = 28;
-					enemyy1 = 10;
+					enemyy1 = 5;
 					x1 = RandomWord();
-					
-
+				}
+				if (numofcorrect != n1)
+				{
+					numofcorrect = 0;
+					memset(userinput, 0, MAXC * sizeof(char));
+					nextchar = 0;
 				}
 			}
 
@@ -241,9 +295,10 @@ void game_update(void)
 				nextchar = 0;
 			}
 		}
-
-		if (playery == 5)
+		
+		if (playery == 10)
 		{
+
 			for (int check = 0; check < lenofinput; check++)
 			{
 				if (intvalue2[check] == (int)userinput[check])
@@ -258,14 +313,19 @@ void game_update(void)
 				{
 					numofcorrect = 0;
 					score++;
+					numkilled++;
 					memset(userinput, 0, MAXC * sizeof(char));
 					memset(string2, 0, MAXC * sizeof(char));
 					nextchar = 0;
 					enemyx2 = 28;
-					enemyy2 = 5;
+					enemyy2 = 10;
 					x2 = RandomWord();
-
-
+				}
+				if (numofcorrect != n2)
+				{
+					numofcorrect = 0;
+					memset(userinput, 0, MAXC * sizeof(char));
+					nextchar = 0;
 				}
 			}
 
@@ -277,7 +337,47 @@ void game_update(void)
 			}
 		}
 
-		}
+		if (playery == 15)
+		{
+
+			for (int check = 0; check < lenofinput; check++)
+			{
+				if (intvalue3[check] == (int)userinput[check])
+				{
+					numofcorrect++;
+				}
+
+			}
+			if (n3 == lenofinput)
+			{
+				if (numofcorrect == n3)
+				{
+					numofcorrect = 0;
+					score++;
+					numkilled++;
+					memset(userinput, 0, MAXC * sizeof(char));
+					memset(string3, 0, MAXC * sizeof(char));
+					nextchar = 0;
+					enemyx3 = 28;
+					enemyy3 = 15;
+					x3 = RandomWord();
+				}
+				if (numofcorrect != n3)
+				{
+					numofcorrect = 0;
+					memset(userinput, 0, MAXC * sizeof(char));
+					nextchar = 0;
+				}
+			}
+
+			else
+			{
+				numofcorrect = 0;
+				memset(userinput, 0, MAXC * sizeof(char));
+				nextchar = 0;
+			}
+		}	
+	}
 	if (CP_Input_KeyTriggered(KEY_ESCAPE))
 	{
 		CP_Engine_Terminate();
@@ -354,10 +454,33 @@ void ConvertWordToInt(void)
 		intvalue2[i] = (int)*(pstr2 + i);
 
 	}
+	for (i = 0; i < n3; i++)
+	{
+
+		intvalue3[i] = (int)*(pstr3 + i);
+
+	}
 
 
 }
 
+void PlayerMovement(void)
+{
+	if (playery >= 10 && playery <= 15)
+	{
+		if (CP_Input_KeyTriggered(KEY_UP))
+		{
+			playery -= 5;
+		}
+	}
+	if (playery >= 5 && playery <= 10)
+	{
+		if (CP_Input_KeyTriggered(KEY_DOWN))
+		{
+			playery += 5;
+		}
+	}
+}
 void Keyinput(void)
 {
 	//whatever that is typed first is stored in 0 position in array, subsequent char is stored in nth position +1
@@ -498,28 +621,15 @@ void Keyinput(void)
 		nextchar++;
 	}
 	
-	if (playery >= 10 && playery <= 15)
-	{
-		if (CP_Input_KeyTriggered(KEY_UP))
-		{
-				playery -= 5;
-		}
-	}
-	if (playery >= 5 && playery <= 10)
-	{
-		if (CP_Input_KeyTriggered(KEY_DOWN))
-		{
-			playery += 5;
-		}
-	}
+	
 	
 }
 
 void DisplayTime(float timeelapsed, float width, float height)
 {
 	_itoa_s((int)timeelapsed, timebuffer, TIME, 10);
-	CP_Font_DrawText(timebuffer, (float)8 * width, (float)1 * height);
-	CP_Font_DrawText("Time:", (float)5 * width, (float)1 * height);
+	CP_Font_DrawText(timebuffer, (float)5 * width, (float)1 * height);
+	CP_Font_DrawText("Time:", (float)2 * width, (float)1 * height);
 
 }
 
@@ -528,11 +638,19 @@ float GetFinalTime(void)
 	return finaltime;
 }
 
+void DisplayLives(int livesleft, float width, float height)
+{
+	_itoa_s(lives, lifebuffer, LIVES, 10);
+	CP_Font_DrawText(lifebuffer, (float)12 * width, (float)1 * height);
+	CP_Font_DrawText("Lives Left:", (float)8 * width, (float)1 * height);
+
+}
+
 void DisplayScore(int currentscore, float width, float height)
 {
 	_itoa_s(score, scorebuffer, SCORE, 10);
-	CP_Font_DrawText(scorebuffer, (float)26 * width, (float)1 * height);
-	CP_Font_DrawText("Score:", (float)23 * width, (float)1 * height);
+	CP_Font_DrawText(scorebuffer, (float)18 * width, (float)1 * height);
+	CP_Font_DrawText("Score:", (float)15 * width, (float)1 * height);
 
 }
 
@@ -541,13 +659,15 @@ int GetFinalScore(void)
 	return finalscore;
 }
 
-void DisplayLives(int livesleft, float width, float height)
+void DisplayNumberOfEnemiesKilled(int numberkilled, float width, float height)
 {
-	_itoa_s(lives, lifebuffer, LIVES, 10);
-	CP_Font_DrawText(lifebuffer, (float)17 * width, (float)1 * height);
-	CP_Font_DrawText("Lives Left:", (float)13 * width, (float)1 * height);
+	_itoa_s(numkilled, killedbuffer, KILLS, 10);
+	CP_Font_DrawText(killedbuffer, (float)26 * width, (float)1 * height);
+	CP_Font_DrawText("Enemies Killed:", (float)21 * width, (float)1 * height);
 
 }
+
+
 
 
 
